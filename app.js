974424e2319166eacd6b53816c37fe0ec2224d03
timotheG <gonin.timothe@gmail.com>
2024-04-11 17:42:43 +0200

    POST creating route

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader(
@@ -15,7 +17,14 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use("/api/stuff", (req, res, next) => {
+app.post("/api/stuff", (req, res, next) => {
+	console.log(req.body);
+	res.status(201).json({
+		message: "Objet crée !",
+	});
+});
+
+app.get("/api/stuff", (req, res, next) => {
 	const stuff = [
 		{
 			_id: "oeihfzeoi",
