878364db6a51ea60680e0948ad1bedbbe0fb164d
timotheG <gonin.timothe@gmail.com>
2024-04-11 14:59:35 +0200

    GET stuff api

--- app.js
+++ app.js
@@ -2,23 +2,47 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-	console.log("Request received");
-	next();
+app.use("/api/stuff", (req, res, next) => {
+	const stuff = [
+		{
+			_id: "oeihfzeoi",
+			title: "Mon premier objet",
+			description: "Les infos de mon premier objet",
+			imageUrl:
+				"https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+			price: 4900,
+			userId: "qsomihvqios",
+		},
+		{
+			_id: "oeihfzeomoihi",
+			title: "Mon deuxième objet",
+			description: "Les infos de mon deuxième objet",
+			imageUrl:
+				"https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+			price: 2900,
+			userId: "qsomihvqios",
+		},
+	];
+	res.status(200).json(stuff);
 });
 
-app.use((req, res, next) => {
-	res.status(201);
-	next();
-});
+// app.use((req, res, next) => {
+// 	console.log("Request received");
+// 	next();
+// });
 
-app.use((req, res, next) => {
-	res.json({ message: "Your request has been received" });
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.status(201);
+// 	next();
+// });
 
-app.use((req, res, next) => {
-	console.log("Response sent successfully!");
-});
+// app.use((req, res, next) => {
+// 	res.json({ message: "Your request has been received" });
+// 	next();
+// });
+
+// app.use((req, res, next) => {
+// 	console.log("Response sent successfully!");
+// });
 
 module.exports = app;
