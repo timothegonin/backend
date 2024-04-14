ae3b6e3dc12ebaa8314d02fcbc1324b088a54862
timotheG <gonin.timothe@gmail.com>
2024-04-14 16:10:37 +0200

    PUT Thing

--- app.js
+++ app.js
@@ -43,6 +43,12 @@ app.post("/api/stuff", (req, res, next) => {
 		.catch((error) => res.status(400).json({ error }));
 });
 
+app.put("/api/stuff/:id", (req, res, next) => {
+	Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+		.then(() => res.status(200).json({ message: "Objet modifié !" }))
+		.catch(() => res.status(400).json({ error }));
+});
+
 app.get("/api/stuff/:id", (req, res, next) => {
 	Thing.findOne({ _id: req.params.id })
 		.then((thing) => res.status(200).json(thing))
