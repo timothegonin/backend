6d6026006451612b3eb33391f9488ec04197f0f1
timotheG <gonin.timothe@gmail.com>
2024-04-14 15:27:28 +0200

    GET findOne Thing

--- app.js
+++ app.js
@@ -43,6 +43,12 @@ app.post("/api/stuff", (req, res, next) => {
 		.catch((error) => res.status(400).json({ error }));
 });
 
+app.get("/api/stuff/:id", (req, res, next) => {
+	Thing.findOne({ _id: req.params.id })
+		.then((thing) => res.status(200).json(thing))
+		.catch((error) => res.status(404).json({ error }));
+});
+
 app.get("/api/stuff", (req, res, next) => {
 	// const stuff = [
 	// 	{
