fcc666aa6604145c79e04c309e4a74c80ef409a0
timotheG <gonin.timothe@gmail.com>
2024-04-14 14:00:02 +0200

    Thing to database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Thing = require("./models/thing");
 require("dotenv").config();
 
 const app = express();
@@ -32,10 +33,14 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/stuff", (req, res, next) => {
-	console.log(req.body);
-	res.status(201).json({
-		message: "Objet crée !",
+	delete req.body._id;
+	const thing = new Thing({
+		...req.body,
 	});
+	thing
+		.save()
+		.then(() => res.status(201).json({ message: "Objet enregistré !" }))
+		.catch((error) => res.status(400).json({ error }));
 });
 
 app.get("/api/stuff", (req, res, next) => {
