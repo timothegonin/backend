f76d22c602266e1f327931fbb65fa82297b1648e
timotheG <gonin.timothe@gmail.com>
2024-04-17 10:36:25 +0200

    🎨 controller config

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,40 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Thing = require("../models/thing");
+const stuffControllers = require("../controllers/stuff");
 
-router.post("/", (req, res, next) => {
-	delete req.body._id;
-	const thing = new Thing({
-		...req.body,
-	});
-	thing
-		.save()
-		.then(() => res.status(201).json({ message: "Objet enregistré !" }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-router.put("/:id", (req, res, next) => {
-	Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-		.then(() => res.status(200).json({ message: "Objet modifié !" }))
-		.catch(() => res.status(400).json({ error }));
-});
-
-router.delete("/:id", (req, res, next) => {
-	Thing.deleteOne({ _id: req.params.id })
-		.then(() => res.status(200).json({ message: "Objet supprimé !" }))
-		.catch(() => res.status(400).json({ error }));
-});
-
-router.get("/:id", (req, res, next) => {
-	Thing.findOne({ _id: req.params.id })
-		.then((thing) => res.status(200).json(thing))
-		.catch((error) => res.status(404).json({ error }));
-});
-
-router.get("/", (req, res, next) => {
-	Thing.find()
-		.then((things) => res.status(200).json(things))
-		.catch((error) => res.status(400).json({ error }));
-});
+router.post("/", stuffControllers.createThing);
+router.get("/", stuffControllers.getAllStuff);
+router.get("/:id", stuffControllers.getOneThing);
+router.put("/:id", stuffControllers.modifyThing);
+router.delete("/:id", stuffControllers.deleteThing);
 
 module.exports = router;
