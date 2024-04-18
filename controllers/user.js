6aa23c46ee74df17a2556b9f117990794366ed18
timotheG <gonin.timothe@gmail.com>
2024-04-18 19:24:36 +0200

    login function

--- controllers/user.js
+++ controllers/user.js
@@ -17,4 +17,28 @@ exports.signup = (req, res, next) => {
 		.catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+	User.findOne({ email: req.body.email })
+		.then((user) => {
+			if (!user) {
+				return res
+					.status(401)
+					.json({ message: "Paire login/mot de passe incorrecte" });
+			}
+			bcrypt
+				.compare(req.body.password, user.password)
+				.then((valid) => {
+					if (!valid) {
+						return res
+							.status(401)
+							.json({ message: "Paire login/mot de passe incorrecte" });
+					}
+					res.status(200).json({
+						userId: user._id,
+						token: "TOKEN",
+					});
+				})
+				.catch((error) => res.status(500).json({ error }));
+		})
+		.catch((error) => res.status(500).json({ error }));
+};
