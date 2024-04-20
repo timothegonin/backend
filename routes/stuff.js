fe98fb40a14a6fd6afe04a964dd1e421319d92cd
timotheG <gonin.timothe@gmail.com>
2024-04-20 09:38:33 +0200

    auth middleware

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
 const stuffControllers = require("../controllers/stuff");
 
-router.post("/", stuffControllers.createThing);
-router.get("/", stuffControllers.getAllStuff);
-router.get("/:id", stuffControllers.getOneThing);
-router.put("/:id", stuffControllers.modifyThing);
-router.delete("/:id", stuffControllers.deleteThing);
+router.post("/", auth, stuffControllers.createThing);
+router.get("/", auth, stuffControllers.getAllStuff);
+router.get("/:id", auth, stuffControllers.getOneThing);
+router.put("/:id", auth, stuffControllers.modifyThing);
+router.delete("/:id", auth, stuffControllers.deleteThing);
 
 module.exports = router;
